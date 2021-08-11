3263ac096a730f2ca9744e83a028a01e5d339df0
bluninjadragon <bluninjadragon@gmail.com>
2021-08-11 17:14:41 -0500

    added inquirer logic

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const inquirer = require("inquirer");
 const db = require("./db");
 
 const start = () => {
-  inquirer
-    .prompt({
+  inquirer.prompt([{
       name: "postOrBid",
       type: "list",
       message: "Would you like to [POST] an auction or [BID] on an auction?",
       choices: ["POST", "BID", "EXIT"],
-    })
+    }])
     .then((answer) => {
       // based on their answer, either call the bid or the post functions
       if (answer.postOrBid === "POST") {
